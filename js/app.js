fe7ec9b441ce395d3e3088f655ebe82f2d93bd31
FDMcreative <federico.delmonte@gmail.com>
2018-06-10 12:34:56 +0200

    put spaces in display for readability

--- js/app.js
+++ js/app.js
@@ -1,30 +1,61 @@
 $(() => {
   // console.log('workoddio');
 
-  $cards = $('.cards');
-  $card = $('.card');
-  $start = $('#start');
-  $reset = $('#reset');
-  $items = $('#items');
-  $wrongs = $('#wrongs');
-  $display = $('#display');
+  //VARIABLES & CONSTANTS
+
+  const $cards = $('.cards');
+  const $card = $('.card');
+  const $start = $('#start');
+  const $reset = $('#reset');
+  const $items = $('#items');
+  const $wrongs = $('#wrongs');
+  const $display = $('#display');
   let divIndex = 1;
-  var twoPicks = [];
   let i = 0;
-  var lost = 0;
+  let lost = 0;
   let wrong = 0;
-  var yourItems = [];
+  let twoPicks = [];
+  let yourItems = [];
   let storeDivs = [];
 
-
-
-  var deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
+  let deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
   // console.log(deckCards);
 
-  var randomNumber = Math.floor(Math.random() * deckCards.length);
+  // let randomNumber = Math.floor(Math.random() * deckCards.length);
 
-  //This Starts the Game
-  $start.on('click', () => {
+  //FUNCTIONS
+
+  //This sets the 2 divs back to original state AND reset storeDivs
+  function storeDivsReset () {
+    console.log('wrong')
+    $(storeDivs[0]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
+    $(storeDivs[1]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
+    storeDivs = [];
+  };
+
+  //This Resets the Game
+  function reset() {
+    //This hides the cards
+    $cards.hide();
+    //This resets DISPLAY
+    $display.show();
+    //sets back the ace of spades
+    $card.css({ "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')" });
+    divIndex = 1;
+    $card.text('');
+    deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
+    $card.css({"pointer-events": "auto","background-color":"white"});
+    $card.off();
+    $items.text('');
+    console.log('RESeeET');
+    $wrongs.text(0)
+    wrong = 0;
+    lost = 0;
+    yourItems = [];
+  };
+
+  //This starts the Game
+  function start() {
 
     //This shows the cards
     $cards.css({"display":"block"});
@@ -33,9 +64,9 @@ $(() => {
     while (deckCards.length > 0) {
 
       //This gets a random Element from the card deck and its Index
-      randomNumber = Math.floor(Math.random() * deckCards.length);
-      randomCard = deckCards[randomNumber];
-      var indexArray = deckCards.indexOf(deckCards[randomNumber]);
+      let randomNumber = Math.floor(Math.random() * deckCards.length);
+      let randomCard = deckCards[randomNumber];
+      let indexArray = deckCards.indexOf(deckCards[randomNumber]);
       // console.log(randomCard, indexArray);
 
       //removes that Element from the Deck
@@ -43,7 +74,7 @@ $(() => {
       // console.log('remaining cards: ' + deckCards);
 
       //puts that Element inside a Div and advances to the next one
-      var div = ('#' + 'card' + divIndex);
+      let div = ('#' + 'card' + divIndex);
       // console.log(div);
       $(div).text(randomCard);
       divIndex++;
@@ -56,7 +87,7 @@ $(() => {
       //This removes the ace of spades
       $(e.target).css({"background-image": "none"});
       //This brings the text in the foreground
-      $(e.target).css({"text-indent": "1px"});
+      // $(e.target).css({"text-indent": "1px"});
       //This sets the image
       let url = ('./images/' + $(e.target).text() + '.jpg');
       // console.log(url);
@@ -81,21 +112,13 @@ $(() => {
         if (twoPicks[0] === twoPicks[1]) {
           console.log('right');
           yourItems.push(twoPicks[0]);
-          $items.text(yourItems);
+          $items.text(yourItems.join(', '));
           twoPicks = [];
           i = 0;
           storeDivs = [];
         }
         else {
-
           setTimeout(storeDivsReset, 1000);
-          //This sets the 2 divs back to original state AND reset storeDivs
-          function storeDivsReset () {
-            console.log('wrong')
-            $(storeDivs[0]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
-            $(storeDivs[1]).css({"pointer-events": "auto","background-color":"white", "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')"});
-            storeDivs = [];
-          };
 
           twoPicks = [];
           i = 0;
@@ -111,36 +134,23 @@ $(() => {
           $display.hide();
           // $('#display').text('You Lost');
           $card.css({"pointer-events":"none"});
-          reset();
+          setTimeout(reset, 1000);
         }
       }
 
     });
+  };
 
-  })
+  //EVENT LISTENERS
+
+  //This Starts the Game
+  $start.on('click', () => {
+    start();
+  });
 
   // This Resets the Game
   $reset.on('click', () =>{
     reset();
   });
-
-  function reset() {
-    //This hides the cards
-    $cards.hide();
-    //This resets DISPLAY
-    $display.show();
-    //sets back the ace of spades
-    $card.css({ "text-indent": "-9999px", "background-image": "url('./images/ace-of-spade.jpg')" });
-    divIndex = 1;
-    $card.text('');
-    deckCards = ['flower', 'flower', 'flower', 'flower', 'mushroom', 'mushroom','mushroom','mushroom', 'star', 'star', 'star', 'star', '1up', '1up', '10-coins','10-coins', '20-coins', '20-coins'];
-    $card.css({"pointer-events": "auto","background-color":"white"});
-    $card.off();
-    $items.text('');
-    console.log('RESeeET');
-    $wrongs.text(0)
-    wrong = 0;
-    lost = 0;
-  };
 
 });
